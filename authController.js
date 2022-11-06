a96fcf1bbf121f7424ebfc16dec0fdb7d53ace31
Гриньков Никита Андреевич <Grinkov-N@gaz-is.ru>
2022-11-06 12:46:19 +0300

    Created authRouter

---
+++ authController.js
@@ -0,0 +1,27 @@
+class authController {
+    async registration(req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async login(req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getUsers(req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+export default new authController();
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import authRouter from './authRouter.js';
 
 const app = express();
 app.use(express.json());
+app.use('/auth', authRouter);
 
 const PORT = process.env.PORT || 5000;
+const URL = 'mongodb+srv://nick:nick@cluster0.fi2cpih.mongodb.net/?retryWrites=true&w=majority';
 
 const start = async () => {
     try {
+        await mongoose.connect(URL);
         app.listen(PORT, () => {
             console.log(`Server started on ${PORT}`)
         })
