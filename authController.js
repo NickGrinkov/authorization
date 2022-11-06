8eefd9023f08e1557b92e6ee8723937f2f524c4f
Гриньков Никита Андреевич <Grinkov-N@gaz-is.ru>
2022-11-06 14:04:53 +0300

    Set username and password validation

--- authController.js
+++ authController.js
@@ -1,10 +1,15 @@
 import User from "./models/User.js";
 import Role from "./models/Role.js";
 import bcrypt from 'bcryptjs';
+import { validationResult } from 'express-validator';
 
 class AuthController {
     async registration(req, res) {
         try {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()) {
+                res.status(400).json({ message: 'Ошибка при регистрации', errors })
+            }
             const { username, password } = req.body;
             const candidate = await User.findOne({ username });
             if(candidate) {
--- authRouter.js
+++ authRouter.js
@@ -1,10 +1,20 @@
-import Router from 'express';
-import controller from './authController.js';
+import Router from "express";
+import controller from "./authController.js";
+import { check } from "express-validator";
 
 const router = new Router();
 
-router.post('/registration', controller.registration);
-router.post('/login', controller.login);
-router.get('/users', controller.getUsers);
+router.post(
+  "/registration",
+  [
+    check("username", "Имя пользователя не может быть пустым").notEmpty(),
+    check("password", "Пароль должен быть более 4 и менее 16 символов")
+      .notEmpty()
+      .isLength({ min: 5, max: 15 }),
+  ],
+  controller.registration
+);
+router.post("/login", controller.login);
+router.get("/users", controller.getUsers);
 
 export default router;
